5fdb299e0b66562a68e8eb3ab8d91c6f9b74a659
Sam Tureski <gpiggies@gmail.com>
2021-08-30 17:44:38 +0200

    delete everything but tex

--- run.bat
+++
@@ -1,9 +0,0 @@
-python parse_results.py models\dialects nordnorsk all 10
-python parse_results.py models\dialects oestnorsk all 10
-python parse_results.py models\dialects vestnorsk all 10
-python parse_results.py models\dialects troendersk all 10
-
-python correlate_results.py models\dialects\importance_values_sqrt_nordnorsk_all_sorted.tsv models\dialects\features-correlated.tsv
-python correlate_results.py models\dialects\importance_values_sqrt_vestnorsk_all_sorted.tsv models\dialects\features-correlated.tsv
-python correlate_results.py models\dialects\importance_values_sqrt_oestnorsk_all_sorted.tsv models\dialects\features-correlated.tsv
-python correlate_results.py models\dialects\importance_values_sqrt_troendersk_all_sorted.tsv models\dialects\features-correlated.tsv
